701d6f2c937b97a032bb143f5b61760265c1380f
FireProj <morganmalavasi@gmail.com>
2026-05-12 09:03:01 +0200

    buca 2 extract method 2tiri

--- src/bustapaga/buca02/BustaPaga.java
+++ src/bustapaga/buca02/BustaPaga.java
@@ -7,14 +7,14 @@ public class BustaPaga {
         this.imponibile = imponibile;
     }
 
-    public double getTasse() {
+    public double getNetto() {
         final double primoScaglione = Math.max(Math.min(imponibile, 20000.0) - 5000, 0.0);
         final double secondoScaglione = Math.max(Math.min(imponibile, 40000) - 20000, 0.0);
         final double terzoScaglione = Math.max(imponibile - 40000, 0.0);
-        return imponibile - getNetto(primoScaglione, secondoScaglione, terzoScaglione);
+        return imponibile - getTasse(primoScaglione, secondoScaglione, terzoScaglione);
     }
 
-    private static double getNetto(double primoScaglione, double secondoScaglione, double terzoScaglione) {
+    private static double getTasse(double primoScaglione, double secondoScaglione, double terzoScaglione) {
         return primoScaglione * 0.1 + secondoScaglione * 0.2 + terzoScaglione * 0.4;
     }
 }
--- src/bustapaga/buca02/BustaPagaTest02.java
+++ src/bustapaga/buca02/BustaPagaTest02.java
@@ -7,33 +7,33 @@ public class BustaPagaTest02 {
     @Test
     public void tasseZeroSeImponibileInferioreAlPrimoScaglione() {
         BustaPaga bustaPaga = new BustaPaga(5000);
-        assertEquals(5000, bustaPaga.getTasse(), 1e-6);
+        assertEquals(5000, bustaPaga.getNetto(), 1e-6);
     }
 
     @Test
     public void tasseDelPrimoScaglioneEsclusoZonaEsentasse() {
         final BustaPaga bustaPaga = new BustaPaga(10000);
-        assertEquals(9500, bustaPaga.getTasse(), 1e-6);
+        assertEquals(9500, bustaPaga.getNetto(), 1e-6);
 
         final BustaPaga bustaPaga2 = new BustaPaga(20000);
-        assertEquals(18500, bustaPaga2.getTasse(), 1e-6);
+        assertEquals(18500, bustaPaga2.getNetto(), 1e-6);
     }
 
     @Test
     public void tasseDelSecondoScaglioneComeSommaDelleTasseDelPrimoScaglioneEDelSecondoScaglione() {
         final BustaPaga bustaPaga = new BustaPaga(25000);
-        assertEquals(22500, bustaPaga.getTasse(), 1e-6);
+        assertEquals(22500, bustaPaga.getNetto(), 1e-6);
 
         final BustaPaga bustaPaga2 = new BustaPaga(40000);
-        assertEquals(34500, bustaPaga2.getTasse(), 1e-6);
+        assertEquals(34500, bustaPaga2.getNetto(), 1e-6);
     }
 
     @Test
     public void tasseDelTerzoScaglioneComeSommaDelleTasseDiTuttiGliScaglioni() {
         final BustaPaga bustaPaga = new BustaPaga(50000);
-        assertEquals(40500, bustaPaga.getTasse(), 1e-6);
+        assertEquals(40500, bustaPaga.getNetto(), 1e-6);
 
         final BustaPaga bustaPaga2 = new BustaPaga(60000);
-        assertEquals(46500, bustaPaga2.getTasse(), 1e-6);
+        assertEquals(46500, bustaPaga2.getNetto(), 1e-6);
     }
 }
